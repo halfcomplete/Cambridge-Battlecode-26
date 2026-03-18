ba1c95732c13c2aa60a469a63f6829575ac9d0db
Eric <ehu3001@gmail.com>
2026-03-18 21:21:56 +0000

    add get_all_tile_positions_in_range

--- bots/v1/logic_utils.py
+++ bots/v1/logic_utils.py
@@ -3,9 +3,13 @@ import math
 # Ignore the import error for cambc since it's only available in the battlecode environment
 from cambc import Controller, Direction, EntityType, Environment, Position # type: ignore
 
-def get_all_tiles_in_range(r) -> list[Position]:
+def get_all_deltas_in_range(r: float) -> list:
     """
-    Get all possible deltas (x, y) such that x^2 + y^2 <= range.
+    Get all deltas of the format (x, y) such that x^2 + y^2 <= range.
+    Note:
+        This returns a list of tile position DELTAS within the range, NOT absolute tile positions.
+        If you want a list of absolute tile positions within a range, centred around a given position,
+        use `get_all_tile_positions_in_range(r, p)`.
     """
     pairs = []
     limit = int(math.sqrt(r))
@@ -15,4 +19,14 @@ def get_all_tiles_in_range(r) -> list[Position]:
             if x * x + y * y <= r:
                 pairs.append((x, y))
 
-    return [Position(x, y) for x, y in pairs]
+    return [(x, y) for x, y in pairs]
+
+def get_all_tile_positions_in_range(r: float, p: Position) -> list:
+    """
+    Get all tile positions within a given range of a given position.
+    Note:
+        This returns a list of absolute tile positions within the range, centred around a given position.
+        If you want a list of tile position DELTAS within the range, use `get_all_deltas_in_range(r)`.
+    """
+    deltas = get_all_deltas_in_range(r)
+    return [Position(p.x + dx, p.y + dy) for dx, dy in deltas]
