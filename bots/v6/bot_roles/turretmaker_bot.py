17bc73a8a60b3595a250700d3cd19c42709ee293
Oscar Sheng <oscar08.sheng@gmail.com>
2026-04-07 09:45:53 +1000

    Updated Turretmaker

--- bots/v6/bot_roles/turretmaker_bot.py
+++ bots/v6/bot_roles/turretmaker_bot.py
@@ -2,45 +2,68 @@
 
 import random
 
-from cambc import  Controller, Direction, EntityType, Environment, Position
-
+from cambc import  Controller, Direction, EntityType, Environment, Position, GameError
+import pathfinding as pfind
 from bot_roles.builder_base import BuilderBase
 
 class TurretmakerBot(BuilderBase):
+  def __init__(self):
+    super().__init__()
+    self.possibleEnemyCorePositions = []
+  
+  
+  
   def run(self, ct: Controller) -> None:
     # # make sure we can afford a harvester, and not just wander around spamming conveyors
     # # random is to make sure bots run later during the turn don't get entirely excluded by earlier bots using up the Titanium
     # if ct.get_global_resources()[0] < ct.get_harvester_cost()[0] * (1 + random.random() / 4):
     #   return
 
-    # move logic
+
+    #find enemy core
+    nearby_tiles = ct.get_nearby_tiles()
+    for potentialPos in self.possibleEnemyCorePositions:
+      if potentialPos not in nearby_tiles:
+        continue
+      building = ct.get_tile_building_id(potentialPos)
+      if(building != EntityType.CORE or ct.get_team()==ct.get_team(building)):
+        self.possibleEnemyCorePositions.remove(potentialPos)
+        break
+      if( ct.get_entity_type(building)==EntityType.CORE and ct.get_team()!=ct.get_team(building)):
+        self.possibleEnemyCorePositions = [potentialPos]
+
+      
+    if self.tryBuildGunner(ct):
+      return
+
+    
+    
     i = 0
     while True:
-      pos = ct.get_position()
       builtForward = False
-      movePos = pos.add(self.moveDir)
+      movePos = ct.get_position().add(self.moveDir)
 
-      # Get all entities and target enemy cores
-      entList = ct.get_nearby_entities()
-      print(entList)
-      for entity in entList:
-        entityType = ct.get_entity_type(entity)
-        if entityType == EntityType.CORE:
-          turretLoc = ct.get_position(entity)
-          if ct.can_build_gunner(pos, pos.direction_to(turretLoc)):
-            ct.build_gunner(pos, pos.direction_to(turretLoc))
+      # Move away from enemy territory
+      if self.isEnemyAt(ct, movePos):
+        self.changeMoveDir()
+        continue
 
-      # # Move away from enemy territory
-      # if self.isEnemyAt(ct, movePos):
-      #   self.changeMoveDir()
-      #   continue
-
-      if ct.can_build_road(movePos):
-        ct.build_road(movePos)
+      if ct.can_build_conveyor(movePos, self.revDir):
+        ct.build_conveyor(movePos, self.revDir)
 
         sideDir = self.moveDir.rotate_right().rotate_right()
         sidePos = movePos.add(sideDir)
 
+        if ct.can_build_conveyor(sidePos, self.revDir) and self.doublePlaced < 8:
+          ct.build_conveyor(sidePos, self.revDir)
+          self.doublePlaced += 1
+          builtForward = True
+      elif self.posIsInbounds(ct, movePos) and ct.get_entity_type(ct.get_tile_building_id(movePos)) == EntityType.ROAD and ct.get_team(ct.get_tile_building_id(movePos)) == ct.get_team():
+        ct.destroy(movePos)
+        try: 
+          ct.build_conveyor(movePos, self.revDir)
+        except GameError:
+          print("Failed to build conveyor after destroying road, skipping turn")
       elif ct.can_move(self.moveDir):
         # THIS IS BROKEN AND NEEDS MORE TESTING
         # can't build but can move means we are conveyor following
@@ -66,3 +89,67 @@ class TurretmakerBot(BuilderBase):
       if i > 5:
         return
       i += 1
+
+
+
+  def tryBuildGunner(self, ct: Controller):
+    i = 0
+    xAve = 0
+    yAve = 0
+    for potPos in self.possibleEnemyCorePositions:
+      xAve += potPos.x
+      yAve += potPos.y
+      i+=1
+    if len(self.possibleEnemyCorePositions) == 0:
+      return False
+    xAve /= i
+    yAve /= i
+    aimingPos = Position(xAve,yAve)
+    print(self.possibleEnemyCorePositions)
+    print(aimingPos)
+
+    #is the core nearby
+    nearbyBuildingIds = ct.get_nearby_buildings()
+    core = None
+    for buildingId in nearbyBuildingIds:
+      if ct.get_entity_type(buildingId) == EntityType.CORE:
+        core = buildingId
+    if not core:
+      return False
+    
+    #get all possible places we can build right now
+    myPos = ct.get_position()
+    buildablePositions = []
+    for dir in pfind.OCT_DIRECTIONS:
+      testPos = myPos.add(dir)
+      if not self.posIsInbounds(ct, testPos):
+        continue
+      if ct.get_tile_env(testPos) != Environment.EMPTY or ct.get_tile_env(testPos) != Environment.CONVEYOR or not ct.can_build_gunner(testPos, testPos.direction_to(aimingPos)):
+        continue
+      buildablePositions.append(testPos)
+
+    if len(buildablePositions) == 0:
+      return False
+    print("hey3")
+
+    corePos = ct.get_position(core)
+    for dir in pfind.QUAD_DIRECTIONS:
+      testPos = corePos.add(dir).add(dir)
+      if testPos in buildablePositions:
+        if ct.get_tile_env(testPos) == Environment.CONVEYOR:
+          ct.destroy(testPos)
+        print("hey67", testPos)
+        ct.build_gunner(testPos, testPos.direction_to(aimingPos))
+        return True
+      testPos = corePos.add(dir).add(dir).add(dir)
+      if testPos in buildablePositions:
+        if ct.get_tile_env(testPos) == Environment.CONVEYOR:
+          ct.destroy(testPos)
+        print("hey67", testPos)
+        ct.build_gunner(testPos, testPos.direction_to(aimingPos))
+        return True
+
+#cambc run --watch v6 dummy Gunnertest2
+
+
+
